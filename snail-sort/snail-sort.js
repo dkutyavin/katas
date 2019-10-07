7523e39c036be2bdf7707ded74c80d97e22da2f6
kutyavin_de <kutyavin_de@ursip.ru>
2019-10-07 18:24:38 +0300

    feat(tests): add tests for snail-sort

---
+++ snail-sort/snail-sort.js
@@ -0,0 +1,12 @@
+/**
+ * @param {Array<Array<number>>} array
+ * @description Given an n x n array, return the array elements arranged
+ * from outermost elements to the middle element, traveling clockwise.
+ * @see https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
+ */
+
+function snailSort(array) {
+  return [];
+}
+
+module.exports = snailSort;
