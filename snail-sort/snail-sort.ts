7a8bc1e769ea0b4234e0201f825b1553bc984968
kutyavin_de <kutyavin_de@ursip.ru>
2019-10-08 12:18:38 +0300

    chore: make it work

--- snail-sort/snail-sort.ts
+++ snail-sort/snail-sort.ts
@@ -1,19 +1,53 @@
 /**
- * @param {Array<Array<number>>} matrix
  * @description Given an N x N array, return the array elements arranged
  * from outermost elements to the middle element, traveling clockwise.
  * @see https://www.codewars.com/kata/521c2db8ddc89b9b7a0000c1/train/javascript
  */
 
-function snailSort(matrix: Array<Array<number>>) {
-  if (!isSquareMatrix(matrix)) throw new Error("Matrix should be square");
-
-  return [];
-}
-
-function isSquareMatrix(matrix: Array<Array<unknown>>): boolean {
-  const matrixLength = matrix.length;
-  return matrix.every(row => row.length === matrixLength);
-}
-
 module.exports = snailSort;
+
+function snailSort(matrix: Matrix): Array<number> {
+  if (matrix.length === 0) return [];
+  if (matrix.length === 1) return matrix[0];
+
+  const [matrixBorder, matrixWithoutBorder] = separateBorderFromMatrix(matrix);
+  return [...matrixBorder, ...snailSort(matrixWithoutBorder)];
+}
+
+function separateBorderFromMatrix(matrix: Matrix): [Array<number>, Matrix] {
+  let border: Array<number> = [];
+  let matrixWithoutBorder: Matrix = [...matrix];
+
+  // get first row
+  border = [...border, ...matrix[0]];
+  matrixWithoutBorder = matrixWithoutBorder.slice(1);
+
+  // get last column
+  border = [...border, ...matrixWithoutBorder.map(row => row[row.length - 1])];
+  matrixWithoutBorder = matrixWithoutBorder.map(row => {
+    return row.slice(0, row.length - 1);
+  });
+
+  // get last row
+  const lastRow = [...matrixWithoutBorder][
+    matrixWithoutBorder.length - 1
+  ].reverse();
+  border = [...border, ...lastRow];
+  matrixWithoutBorder = matrixWithoutBorder.slice(
+    0,
+    matrixWithoutBorder.length - 1
+  );
+
+  // get first column
+  if (matrixWithoutBorder.length === 0) return [border, matrixWithoutBorder];
+
+  const firstColumn = [...matrixWithoutBorder].reverse().map(row => row[0]);
+  border = [...border, ...firstColumn];
+  matrixWithoutBorder = matrixWithoutBorder.map(row => {
+    return row.slice(1);
+  });
+
+  return [border, matrixWithoutBorder];
+}
+
+type Matrix = Array<Array<number>>;
