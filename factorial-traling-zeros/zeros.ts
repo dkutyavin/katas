5379b3c96146b28fcb6ad6a0ca69f988fa68443b
kutyavin_de <kutyavin_de@ursip.ru>
2019-10-08 14:21:57 +0300

    chore: write tests

---
+++ factorial-traling-zeros/zeros.ts
@@ -0,0 +1,8 @@
+/**
+ * Write a program that will calculate the number of
+ * trailing zeros in a factorial of a given number.
+ */
+
+module.exports = (n: number): number => {
+  return Math.floor(n / 5);
+};
