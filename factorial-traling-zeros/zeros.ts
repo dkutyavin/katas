c37c4dccc1400bed36c7cfb7b2fc2de73528dca5
kutyavin_de <kutyavin_de@ursip.ru>
2019-10-08 15:15:01 +0300

    feat: add zeroz func

--- factorial-traling-zeros/zeros.ts
+++ factorial-traling-zeros/zeros.ts
@@ -1,8 +1,16 @@
 /**
  * Write a program that will calculate the number of
  * trailing zeros in a factorial of a given number.
+ * @see https://www.codewars.com/kata/number-of-trailing-zeros-of-n/train/javascript
  */
 
 module.exports = (n: number): number => {
-  return Math.floor(n / 5);
+  const highestFivePower = n >= 5 ? Math.log(n) / Math.log(5) : 0;
+
+  let result = 0;
+  for (let i = 1; i <= highestFivePower; i++) {
+    result += Math.floor(n / 5 ** i);
+  }
+
+  return result;
 };
