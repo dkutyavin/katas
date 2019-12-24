8bb21182f72e61614b933cbea91e6afbc2db2e27
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2019-12-24 14:30:31 +0300

    path: make it work

--- path-finder/path-finder.ts
+++ path-finder/path-finder.ts
@@ -8,30 +8,63 @@
  * `.W.
     .W.
     ...`
- */
+    */
 
-module.exports = (maze: string): boolean => {
-  /*
-  let checked = []
-  let toCheck = [START]
+module.exports = (mazeInput: string) => {
+  const maze = new Maze(mazeInput);
+
+  const checked: Array<Position> = [];
+  let toCheck: Array<Position> = [maze.ENTRANCE];
+
   while (toCheck.length > 0) {
-    let testing = toCheck.pop()
-    let availablePositions = getAvaliablePositions(testing).filterOut(checked)
-    checked = [...checked, testing]
-    
-    if(END in availablePositions) return true
+    const testing = toCheck.pop();
+    const neighbours = maze.neighbours(testing);
+    const paths = maze.nonBlockedPositions(neighbours);
 
-    toCheck = [...toCheck, ...availablePositions]
+    if (paths.some(([x, y]) => x === maze.EXIT[0] && y === maze.EXIT[1]))
+      return true;
+
+    checked.push(testing);
+
+    toCheck.push(
+      ...paths.filter(it => {
+        return !checked.some(([x, y]) => it[0] === x && it[1] === y);
+      })
+    );
   }
-  */
-
   return false;
 };
 
-/*
-function getAvailablePositions(position, maze)
-  const EMPTY = ".";
-  const WALL = "W";
+class Maze {
+  maze: Array<Array<string>>;
+  ENTRANCE: Position = [0, 0];
+  EXIT: Position;
 
-  
-*/
+  constructor(mazeInput: string) {
+    const rows = mazeInput.split("\n");
+    const cells = rows.map(row => row.replace(/\s/g, "").split(""));
+
+    this.maze = cells;
+    this.EXIT = [this.maze.length - 1, this.maze.length - 1];
+  }
+
+  neighbours([x, y]: Position) {
+    const neighbourPositions: Position[] = [
+      [x - 1, y],
+      [x + 1, y],
+      [x, y - 1],
+      [x, y + 1],
+    ];
+
+    return neighbourPositions.filter(
+      ([x, y]) =>
+        x >= 0 && x < this.maze.length && y >= 0 && y < this.maze.length
+    );
+  }
+
+  nonBlockedPositions(positions: Position[]) {
+    return positions.filter(([x, y]) => this.maze[x][y] !== "W");
+  }
+}
+
+type Position = [number, number];
