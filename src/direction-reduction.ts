a48a4b09ea47a07852a1e28e20ef5d6a5a59b2fc
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-01-17 17:41:40 +0300

    chore: refactor dirReduc

--- src/direction-reduction.ts
+++ src/direction-reduction.ts
@@ -3,27 +3,14 @@
   @description Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
  */
 
-export const dirReduc = (directions: Directions): Directions => {
-  let result = [...directions];
-  while (!simplified(result)) result = simplify(result);
-  return result;
-};
+export const dirReduc = (directions: Directions): Directions =>
+  directions.reduce((result, direction) => {
+    if (isOpposite(direction, result[result.length - 1])) {
+      result.pop();
+    } else result.push(direction);
 
-const simplify = (aPath: Directions): Directions => {
-  const result = [];
-  for (let i = 0; i < aPath.length; i++) {
-    if (!isOpposite(aPath[i], aPath[i + 1])) result.push(aPath[i]);
-    else i++;
-  }
-  return result;
-};
-
-const simplified = (aPath: Directions): boolean => {
-  for (let i = 0; i < aPath.length; i++) {
-    if (isOpposite(aPath[i], aPath[i + 1])) return false;
-  }
-  return true;
-};
+    return result;
+  }, []);
 
 const isOpposite = (dir1: Direction, dir2: Direction) => {
   return dir2 === opposites[dir1];
