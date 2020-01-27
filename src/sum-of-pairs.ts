b460b635a2a27d2588c675b8e1ac9367802334aa
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-01-27 18:10:08 +0300

    feat(sum of pairs) make it work

--- src/sum-of-pairs.ts
+++ src/sum-of-pairs.ts
@@ -3,6 +3,11 @@
  * @see https://www.codewars.com/kata/54d81488b981293527000c8f/train/javascript
  */
 
-export const sumPairs = (numbers: Array<Number>, sum: Number) => {
-  
+export const sumPairs = (numbers: Array<number>, sum: number) => {
+  for (let i = 0; i < numbers.length; i++) {
+    const n = numbers[i];
+
+    const arr = numbers.filter((it, index) => it + n === sum && index !== i);
+    if (arr.length) return [n, arr[0]];
+  }
 };
