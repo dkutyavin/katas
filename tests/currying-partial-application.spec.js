55610638158f36dff27a35658fda55ac4bcc88be
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-03-03 18:43:59 +0300

    chore(curry-partial): add use cases

--- tests/currying-partial-application.spec.js
+++ tests/currying-partial-application.spec.js
@@ -14,6 +14,11 @@ describe('Currying and partial application', () => {
       const currySum = curryingPartial(sumOfThree)
       expect(currySum(a)()()(b)()(c)).toBe(sumOfThree(a, b, c))
     })
+
+    test('When given more params, than needed, should return correct result', () => {
+      const currySum = curryingPartial(sumOfThree)
+      expect(currySum(a)(b)(c, a, b, c)).toBe(sumOfThree(a, b, c))
+    })
   })
 
   describe('Partially applied sum of three params', () => {
@@ -23,6 +28,11 @@ describe('Currying and partial application', () => {
     test('When first param is given in the curryingPartial constructor, sum should return correct result', () => {
       const partialSum = curryingPartial(sumOfThree, a)
       expect(partialSum(b)(c)).toBe(sumOfThree(a, b, c))
+    })
+
+    test('When all params are given to curryingPartial, should immediately return result', () => {
+      const fullSum = curryingPartial(sumOfThree, a, b, c)
+      expect(fullSum).toBe(sumOfThree(a, b, c))
     })
   })
 })
