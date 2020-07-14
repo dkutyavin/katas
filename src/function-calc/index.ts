e1deafce53be725b5ad1dceb074901a130921d65
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-07-14 18:35:53 +0300

    function-calc: refactor

--- src/function-calc/index.ts
+++ src/function-calc/index.ts
@@ -10,21 +10,29 @@
  * @see https://www.codewars.com/kata/525f3eda17c7cd9f9e000b39/train/javascript
  */
 
-const number = value => operation => (operation ? operation(value) : value);
-const operation = fn => right => left => fn(left, right);
+const int: Int = (value) => (operation) => {
+  if (typeof operation !== 'function') return value
+  return operation(value)
+}
+const operation: Operation = (fn) => (right) => (left) => fn(left, right)
 
-export const zero = number(0);
-export const one = number(1);
-export const two = number(2);
-export const three = number(3);
-export const four = number(4);
-export const five = number(5);
-export const six = number(6);
-export const seven = number(7);
-export const eight = number(8);
-export const nine = number(9);
+export const zero = int(0)
+export const one = int(1)
+export const two = int(2)
+export const three = int(3)
+export const four = int(4)
+export const five = int(5)
+export const six = int(6)
+export const seven = int(7)
+export const eight = int(8)
+export const nine = int(9)
 
-export const plus = operation((left, right) => left + right);
-export const minus = operation((left, right) => left - right);
-export const times = operation((left, right) => left * right);
-export const dividedBy = operation((left, right) => Math.floor(left / right));
+export const plus = operation((left, right) => left + right)
+export const minus = operation((left, right) => left - right)
+export const times = operation((left, right) => left * right)
+export const dividedBy = operation((left, right) => Math.floor(left / right))
+
+type Operation = (fn: OperationFun) => (right: number) => (left: number) => number
+type OperationFun = (left: number, right: number) => number
+
+type Int = (value: number) => (operation: (value: number) => number) => number
