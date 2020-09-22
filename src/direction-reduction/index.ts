9dc56ab0ccdfbfaa3570cf06fa763767ff21d6b5
Daniil Kutyavin <daniil.kutyavin94@gmail.com>
2020-09-22 13:33:21 +0300

    Direction reduction refactor (#40)

--- src/direction-reduction/index.ts
+++ src/direction-reduction/index.ts
@@ -3,25 +3,31 @@
   @description Write a function dirReduc which will take an array of strings and returns an array of strings with the needless directions removed (W<->E or S<->N side by side).
  */
 
-export const dirReduc = (directions: Directions): Directions =>
-  directions.reduce((result, direction) => {
-    if (isOpposite(direction, result[result.length - 1])) {
-      result.pop();
-    } else result.push(direction);
+export function dirReduc(directions: Direction[]) {
+  let result: Direction[] = []
 
-    return result;
-  }, []);
+  for (const direction of directions) {
+    const prev = result.slice(-1)[0]
 
-const isOpposite = (dir1: Direction, dir2: Direction) => {
-  return dir2 === opposites[dir1];
-};
+    if (isOpposite(direction, prev)) {
+      result.pop()
+    } else {
+      result.push(direction)
+    }
+  }
 
-const opposites: Record<Direction, Direction> = {
-  EAST: "WEST",
-  WEST: "EAST",
-  NORTH: "SOUTH",
-  SOUTH: "NORTH",
-};
+  return result
+}
 
-type Directions = Array<Direction>;
-type Direction = "NORTH" | "SOUTH" | "EAST" | "WEST";
+function isOpposite(dir1: Direction, dir2: Direction) {
+  const opposites: Record<Direction, Direction> = {
+    EAST: 'WEST',
+    WEST: 'EAST',
+    NORTH: 'SOUTH',
+    SOUTH: 'NORTH',
+  }
+
+  return dir2 === opposites[dir1]
+}
+
+type Direction = 'NORTH' | 'SOUTH' | 'EAST' | 'WEST'
